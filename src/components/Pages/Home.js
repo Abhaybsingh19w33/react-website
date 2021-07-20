3e75d999d3d82846f2cfade40a6b5a34f031d0da
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-07-20 12:41:32 +0530

    Services added to home page

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Header from '../Common/Header';
 // import image from '../asset/img/header-bg.jpg';
 // import '../assets/img/header-bg.jpg';
-
+import Services from '../Common/Services';
 class Home extends Component {
     render() {
         return (
@@ -13,17 +13,8 @@ class Home extends Component {
                     buttonText="Tell Me More"
                     link="/services"
                     showButton={true}
-                    // image="../asset/img/header-bg.jpg"
-                    // ../assets/img/header-bg.jpg
-                    // image={image}    
                 />
-                {/* <header className="masthead">
-                    <div className="container">
-                        <div className="masthead-subheading">Welcome To Our Studio!</div>
-                        <div className="masthead-heading text-uppercase">It's Nice To Meet You</div>
-                        <a className="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
-                    </div>
-                </header> */}
+                <Services />
             </div>
         );
     }
