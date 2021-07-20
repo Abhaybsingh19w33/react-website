951046d29b07f0339f071d4e62af152f17414f7f
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-07-20 11:37:47 +0530

    Home page added

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
+import Header from '../Common/Header';
+// import image from '../asset/img/header-bg.jpg';
+// import '../assets/img/header-bg.jpg';
+
 class Home extends Component {
     render() {
         return (
             <div>
-                <header className="masthead">
+                <Header
+                    title="Welcome To Our Studio!"
+                    subtitle="It's Nice To Meet You"
+                    buttonText="Tell Me More"
+                    link="/services"
+                    showButton={true}
+                    // image="../asset/img/header-bg.jpg"
+                    // ../assets/img/header-bg.jpg
+                    // image={image}    
+                />
+                {/* <header className="masthead">
                     <div className="container">
                         <div className="masthead-subheading">Welcome To Our Studio!</div>
                         <div className="masthead-heading text-uppercase">It's Nice To Meet You</div>
                         <a className="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
                     </div>
-                </header>
+                </header> */}
             </div>
         );
     }
